fbbd3ef8d0f9f3ac275d1995bd59b356828867bf
davidvera13 <david.vera@9online.fr>
2023-07-23 11:58:39 +0200

    feat(hibernate): step 14 - fetch operation project implementation: optimization

--- Hibernate and Spring Data JPA/step14-fetch-operations/order-service/src/main/java/com/hibernate/jpa/orders/repository/CustomerRepository.java
+++ Hibernate and Spring Data JPA/step14-fetch-operations/order-service/src/main/java/com/hibernate/jpa/orders/repository/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.hibernate.jpa.orders.repository;
 import com.hibernate.jpa.orders.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByCustomerNameIgnoreCase(String customerName);
 }
--- Hibernate and Spring Data JPA/step14-fetch-operations/order-service/src/main/java/com/hibernate/jpa/orders/repository/ProductRepository.java
+++ Hibernate and Spring Data JPA/step14-fetch-operations/order-service/src/main/java/com/hibernate/jpa/orders/repository/ProductRepository.java
@@ -4,5 +4,8 @@ import com.hibernate.jpa.orders.domain.OrderHeader;
 import com.hibernate.jpa.orders.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByDescription(String description);
 }
