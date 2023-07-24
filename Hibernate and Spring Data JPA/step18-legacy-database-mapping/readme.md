83eb71d9c6f53260f5ebe25d770ab74c497b0e2e
davidvera13 <david.vera@9online.fr>
2023-07-24 20:57:17 +0200

    feat(hibernate): step 18 - Legacy db mapping - reverse engineering, from db to entity

---
+++ Hibernate and Spring Data JPA/step18-legacy-database-mapping/readme.md
@@ -0,0 +1,11 @@
+# Legacy Database Mapping Overview
+
+###
+
+* Legacy Database - An older database, originally not written for use with JPA
+* JPA has the ability to name tables and columns
+* So far in course, we’ve been following default naming conventions
+* firstName = FIRST_NAME
+* Wordpress is a good example of a legacy database - built for PHP application
+* We will use the Wordpress database schema to explore JPAs database mapping features
+
---
+++ Hibernate and Spring Data JPA/step18-legacy-database-mapping/src/main/java/com/hibernate/jpa/domain/User.java
@@ -0,0 +1,168 @@
+package com.hibernate.jpa.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+/**
+ * we can configure table name and column names
+ */
+@Entity
+@Table(
+        name = "wp_users",
+        indexes = {
+                @Index(name = "user_login_key", columnList = "user_login"),
+                @Index(name = "user_nicename", columnList = "user_nicename"),
+                @Index(name = "user_email", columnList = "user_email")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 60)
+    @Column(name = "user_login", length = 60)
+    private String login;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "user_pass", length = 255)
+    private String password;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "user_nicename", length = 100)
+    private String nicename;
+
+    @Email
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "user_email", length = 100)
+    private String email;
+
+
+    @URL
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "user_url")
+    private String url;
+
+    @Column(name = "user_registered")
+    private Timestamp registered;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "user_activation_key")
+    private String activationKey;
+
+    @NotNull
+    @Column(name = "user_status")
+    private Integer status;
+
+    // name is using convention, value is not optional... we can pass optional annotation intead of column
+    // @Column(name = "display_name", nullable = false)
+    @NotNull
+    @Size(max = 255)
+    @Basic(optional = false)
+    private String displayName;
+
+    // Adding Bi-Directional Association
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private Set<UserMeta> userMetaSet;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNicename() {
+        return nicename;
+    }
+
+    public void setNicename(String nicename) {
+        this.nicename = nicename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Timestamp getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Timestamp registered) {
+        this.registered = registered;
+    }
+
+    public String getActivationKey() {
+        return activationKey;
+    }
+
+    public void setActivationKey(String activationKey) {
+        this.activationKey = activationKey;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Set<UserMeta> getUserMetaSet() {
+        return userMetaSet;
+    }
+
+    public void setUserMetaSet(Set<UserMeta> userMetaSet) {
+        this.userMetaSet = userMetaSet;
+    }
+}
